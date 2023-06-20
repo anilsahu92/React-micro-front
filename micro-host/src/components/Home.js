ccdc5b10b3f932f568770043e4dc6b39b64c56ce
anilsahu17 <anilsahu17992@gmail.com>
2023-06-20 14:24:25 +0530

    new changes

---
+++ micro-host/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>This is second app Home</h1>
+      <p>HomeHomeHome HomeHomeHome Home</p>
+    </div>
+  );
+};
+export default Home;
